681604d0a1138345bd6bc166f08f5f8e60ad7079
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2024-01-03 00:14:58 -0500

    Test Front two but not all happy :(

--- client/src/test/tinyTourist.test.jsx
+++ client/src/test/tinyTourist.test.jsx
@@ -1,12 +1,33 @@
 import { render, screen } from "@testing-library/react"
-
+import { BrowserRouter as Router } from "react-router-dom";
+import CardsCountries from "../components/CardsCountries/CardsCountries";
 import Pagination from "../components/Pagination/Pagination";
 
+
+
+
+describe("CardsCountries", ()=> {
+ test("Should render an error message when allCountries is not an array", () => {
+  render(
+    <Router>
+      <CardsCountries allCountries="Not an array" currentPage={1} selectedActivity={false} countriesPerPage={10} />
+    </Router>
+  ); 
+  expect(screen.getByText(/so sorry/i)).toBeTruthy();
+ })
+
+})
+
+ 
 
 describe("Pagination", ()=> {
   test("Should be a render a pagination component", () => {
     render(<Pagination currentPage={1} totalPages={10} PageToBeChange={() => {}} />); 
     expect(screen.getByText('1 of 10')).toBeDefined();
   })
+
+
+  
  })
+  
 
